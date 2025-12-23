fe033128980d136bc658576be713f9ce7d19dc7a
shahdev kumar <shahdevkumar.d@gmail.com>
2025-12-23 16:26:22 +0500

    js

--- example.js
+++ example.js
@@ -126,3 +126,351 @@
 
 //let tic_tac_toe = [['X',null,'O'], [null,'X','O',],['O',null,'X']];
 
+
+
+// for(int i=1;i<=5;i++){
+//     console.log(i);
+// }
+
+
+// js object literals
+// property => (key, value)
+
+// let delhi = {
+//     latitude: "25.23232"
+//     longitude : "555.4323"
+
+// };
+
+// const student = {
+//     name: "Kumar",
+//     age:23,
+//     marks: 34.4,
+//     city: "Delhi",
+// };
+
+
+// const item = {
+//     price: 100.99,
+//     discunt:50,
+//     colors:["red","pink"],
+
+// };
+
+
+// const post ={
+//     username: "Kumar",
+//     content: "this is my #first post",
+//     likes: 50,
+//     reposts: 65,
+//     tags: ["coding", "delta", "programming"],
+// };
+
+
+
+// const obj = {
+//     1:"a",
+//     2:"b",
+//     true: "c",
+//     null:"d",
+//     undefined: "e",
+// };
+
+// object keys to strings
+
+// const student = {
+//     name:"kumar",
+//     age: 23,
+//     city: "Karachi",
+
+// };
+
+
+// object of Objects
+
+// const classInfo = {
+//     aman:{
+//         grade:"A",
+//         city:"Karachi",
+
+//     },
+//     kumar:{
+//         grade:"B",
+//         city:"Hyderabad",
+//     },
+//     dev:{
+//         grade:"C",
+//         city:"Mithi",
+
+//     },
+// };
+
+
+// array of objects
+// const classInfo = [
+//     {
+//      name:"aman",
+//     grade:"A",
+//     city:"Karachi",
+
+//     },
+//     {
+//         name: "kumar",
+//         grade:"B",
+//      city:"Hyderabad",
+
+//     },
+//     {
+//         name:"dev",
+//         grade:"C",
+//         city: "Mithi",
+
+//     },
+
+
+// ];
+
+
+
+
+
+// random number genenrate from 1 to 10
+// let step1 = Math.random();
+
+// let step2 = step1*10;
+// let step3 = Math.floor(step2);
+// let step4 = step3+1;
+
+
+// Math.floor(Math.random()*100)+1;
+
+//const max = prompt("enter the max number");
+//console.log(max);
+
+//const random = Math.floor(Math.random()*max)+1;
+//console.log(random);
+
+
+
+
+// let guess = prompt("guess the number")
+// while(guess){
+//     if(guess == "quit"){
+//             console.log("user quit");
+//             break;
+//     }
+//     if(guess == random){
+//         console.log("u are right");
+//         break;
+//     }
+//     else if(guess< random){
+//         prompt("u guess was too small , please try again");
+//     }
+//     else{
+//         prompt("u guess was too large , please try again");
+//     }
+
+    // else{
+    //     guess = prompt("you guess was wrong. please try again");
+
+    // }
+
+    
+    
+//}
+
+
+
+
+
+//  function hello(){
+//     console.log("Hello")
+//  }
+
+//  function prinName(){
+//     console.log("my name is kumar");
+//  }
+
+//  function printLoop(){
+//     for(let i =1;i<=5;i++){
+//         console.log(i);
+
+//     }
+//  }
+
+//  function isAdult(){
+//     let age =16;
+//     if(age>=16){
+//         console.log("adult");
+//     }
+//     else{
+//         console.log("not adult");
+
+//     }
+//  }
+
+//  function printPoem(){
+//     console.log("Twinkle Twinkel little start");
+//     console.log("Twinkle Twinkel little start");
+//  }
+
+
+// function randomGenerate(){
+//     let random = Math.random(Math.floor()*6)+1;
+//     console.log(random);
+// }
+
+
+// function printInfo(name, age){
+//     console.log(`${name}s age is ${age}`);
+// }
+
+
+// function printTable(n){
+//     for(let i =n;i<=n*10;i+=n){
+//         console.log(i);
+
+//     }
+// }
+
+
+
+// function sum(a,b){
+//     return a+b;
+
+// }
+
+
+
+
+
+//  hello();
+//  prinName();
+//  printLoop();
+//  isAdult();
+//  printPoem();
+//  randomGenerate();
+//printInfo("Kumar", 23);
+//printTable(10);
+
+// sum(3,4); // return this value.
+// let s=sum(3,4);
+// console.log(s);
+
+
+
+// function expression
+
+// let name = "Shadhev"
+// let sum = function(a,b){
+//     return a+b;
+
+// }
+
+// let hello = function(){
+//     console.log("hello");
+// }
+
+
+// function multipleGreet(func, count){ // higher order function 
+
+//     for(let i =1;i<=count;i++){
+//         func();
+
+//     }
+// }
+
+// let greet = function(){
+//     console.log("hello");
+// }
+
+
+// multipleGreet(greet,100);
+
+
+
+
+
+// function oddOrEvenFactory(request){
+//   if(request == "odd"){
+//   return function(){
+//         console.log(!(n%2 == 0));
+    
+//     } 
+
+
+//   }
+//   else if(request == "Even"){
+// return function(){
+//         console.log(n%2 == 0);
+    
+//     }
+ 
+
+
+//   }
+//   else{
+//     console.log("wrong request");
+     
+//   }
+
+// }
+// let request = "odd"
+
+
+// const calculator = {
+//     let num =55;
+
+//     add: function(a,b){
+//         return a+b;
+//     },
+//     sub: function(a,b){
+//         return a-b;
+//     },
+//     mul: function(a,b){
+//         return a*b;
+//     }
+// }
+
+// const calculator = {
+//     add(a,b){
+//         return a+b;
+//     },
+//     sub(a,b){
+//         return a-b;
+//     },
+//     mult(a,b){
+//         return a*b;
+//     }
+// }
+
+
+
+
+// const student = {
+//     name: "Kumar",
+//     age: 23,
+//     phy:54,
+//     getAvg()
+// }
+
+
+
+// 
+
+
+
+// const sum = (a,b)=>{
+//     console.log(a+b);
+// }
+
+// const cube = n=>{
+//     return n*n*n;
+// };
+
+
+console.log("Hi there");
+setTimeout( ()=>{
+    console.log("shahdev kumar ");
+},4000);
+console.log("WElcome to ");
